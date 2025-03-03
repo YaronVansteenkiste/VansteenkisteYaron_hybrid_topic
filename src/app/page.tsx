2d1a3ced65ebfcbbfbeacb8ef6b7aaaa6512ee3f
YaronVansteenkiste <yaron.vansteenkiste@telenet.be>
2025-03-03 18:56:56 +0100

    03/03: lesson 3 simple bar chart

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,19 +7,37 @@ import { useEffect } from "react";
 
 export default function Home() {
   useEffect(() => {
-    const dataset = [1, 2, 3, 4, 5];
+    const dataset = [80, 100, 56, 120, 180, 30, 40, 120, 160];
 
-    d3.select("body")
-      .selectAll("p")
+    const svgWidth = 500;
+    const svgHeight = 300;
+    const barPadding = 5;
+    const barWidth = svgWidth / dataset.length;
+
+
+    const svg = d3.select('svg')
+      .attr('width', svgWidth)
+      .attr('height', svgHeight);
+
+    const barChart = svg.selectAll('rect')
       .data(dataset)
       .enter()
-      .append("p")
-      .text(function(d) {return d;})
+      .append('rect')
+      .attr('y', d => svgHeight - d)
+      .attr('height', d => d)
+      .attr('fill', 'lightblue') 
+      .attr('width', barWidth - barPadding)
+      .attr('transform', (d, i) => {
+        const translate = [barWidth * i, 0];
+        return `translate(${translate})`;
+      }
+      );
   }, []);
 
   return (
     <div>
       <h1>D3js demo</h1>
+      <svg className="bg-dark"></svg>
     </div>
   );
 }
