20c9daed20414056a55d7e1d2853d2576633dfd5
YaronVansteenkiste <yaron.vansteenkiste@telenet.be>
2025-03-03 19:08:40 +0100

    03/03: lesson 7 axis

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,46 +7,36 @@ import { useEffect } from "react";
 
 export default function Home() {
   useEffect(() => {
-    const dataset = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+    const dataset = [80, 100, 56, 120, 180, 30, 40, 120, 160];
 
     const svgWidth = 500;
     const svgHeight = 300;
-    const barPadding = 5;
-    const barWidth = svgWidth / dataset.length;
-
-
+    
     const svg = d3.select('svg')
       .attr('width', svgWidth)
-      .attr('height', svgHeight);
+      .attr('height', svgHeight)
+      .attr('class', 'bg-dark');
 
-    const yScale = d3.scaleLinear()
-      .domain([0, d3.max(dataset) ?? 0])
-      .range([0, svgHeight]);
+      const xScale = d3.scaleLinear()
+        .domain([0, d3.max(dataset) ?? 0])
+        .range([0, svgWidth]);
 
-    const barChart = svg.selectAll('rect')
-      .data(dataset)
-      .enter()
-      .append('rect')
-      .attr('y', d => svgHeight - yScale(d))
-      .attr('height', d => yScale(d))
-      .attr('fill', 'lightblue')
-      .attr('width', barWidth - barPadding)
-      .attr('transform', (d, i) => {
-        const translate = [barWidth * i, 0];
-        return `translate(${translate})`;
-      }
-      );
+      const yScale = d3.scaleLinear()
+        .domain([0, d3.max(dataset) ?? 0])
+        .range([svgHeight, 0]);
 
+      const xAxis = d3.axisBottom(xScale);
+      const yAxis = d3.axisLeft(yScale);
 
-    const text = svg.selectAll('text')
-      .data(dataset)
-      .enter()
-      .append('text')
-      .text(d => d)
-      .attr('y', (d, i) => svgHeight - d - 2)
-      .attr('x', (d, i) => barWidth * i)
-      .attr('fill', '#A64C38');
+      svg.append('g')
+        .attr('transform', 'translate(50, 10)')
+        .call(yAxis);
 
+      const xAxisTranslate = svgHeight - 20;
+
+      svg.append('g')
+        .attr('transform', `translate(50, ${xAxisTranslate})`)
+        .call(xAxis);
   }, []);
 
   return (
