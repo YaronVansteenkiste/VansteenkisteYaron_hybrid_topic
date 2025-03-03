8bdec21ef2a2600919de905782df32af7dbbfbac
YaronVansteenkiste <yaron.vansteenkiste@telenet.be>
2025-03-03 19:59:48 +0100

    03/03: selection and manipulation

--- src/app/components/DonutChart.tsx
+++ src/app/components/DonutChart.tsx
@@ -1,5 +1,22 @@
+'use client';
+
+import * as d3 from "d3";
+import { useEffect, useRef } from "react";
+
 export default function DonutChart() {
-    return (<div>
-        <p>todo</p>
-    </div>)
+
+  useEffect(() => {
+    d3.select('h1')
+        .style('color', 'red')
+        .style('font-size', '2rem')
+        .style('font-weight', 'bold');
+    d3.select('p').style('color', 'blue');
+  }, []);
+
+  return (
+    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', height: '100vh' }}>
+      <h1>D3.js Donut Chart</h1>
+      <p>selection and manipulation</p>
+    </div>
+  );
 }
