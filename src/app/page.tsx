96410c11adabfbf3110aa4a68b1f1be82a5f2c7a
YaronVansteenkiste <yaron.vansteenkiste@telenet.be>
2025-03-03 18:47:54 +0100

    03/03: lesson 1 -> selection and manipulation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,24 @@
+// gebruikte bronnen:
+// https://d3js.org/getting-started
+// https://www.youtube.com/watch?v=C4t6qfHZ6Tw
+'use client';
+import * as d3 from "d3";
+import { useEffect } from "react";
+
 export default function Home() {
+  useEffect(() => {
+    d3.select("h1").style("color", "red")
+      .attr('class', 'heading')
+      .text('Updated h1 tag');
+
+
+    d3.select('body').append('p').text('First paragraph');
+    d3.select('body').append('p').text('Second paragraph');
+    d3.select('body').append('p').text('Third paragraph');
+
+    d3.selectAll('p').style('color', 'blue');
+  }, []);
+
   return (
     <div>
       <h1>D3js demo</h1>
