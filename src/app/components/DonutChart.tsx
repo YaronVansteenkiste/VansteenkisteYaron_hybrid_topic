42cbbdb6776fcd9f5f6c0056f305d81f15c46b4f
YaronVansteenkiste <yaron.vansteenkiste@telenet.be>
2025-03-03 20:02:43 +0100

    03/03: data loading and binding

--- src/app/components/DonutChart.tsx
+++ src/app/components/DonutChart.tsx
@@ -5,18 +5,27 @@ import { useEffect, useRef } from "react";
 
 export default function DonutChart() {
 
-  useEffect(() => {
-    d3.select('h1')
-        .style('color', 'red')
-        .style('font-size', '2rem')
-        .style('font-weight', 'bold');
-    d3.select('p').style('color', 'blue');
-  }, []);
+    useEffect(() => {
+        const data: { framework: string; value: number }[] = [
+          { framework: "Spring i/o", value: 90 },
+          { framework: "NodeJS", value: 90 },
+          { framework: "React", value: 90 },
+          { framework: "Next.js", value: 90 }
+        ];
+
+        d3.select('body')
+        .selectAll('p')
+        .data(data)
+        .enter()
+        .append('p')
+        .text(d => `${d.framework}: ${d.value}`);
+
+      }, []);
 
   return (
-    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', height: '100vh' }}>
+    <div>
       <h1>D3.js Donut Chart</h1>
-      <p>selection and manipulation</p>
+      <svg></svg>
     </div>
   );
 }
